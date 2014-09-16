6f05081366aaf29870fff09bdc822921fab945c0
Deepak Kumar <deepak.s.kumar.85@gmail.com>
2014-09-16 15:12:25 -0400

    Initial setup

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name thetaTauApp
+ * @description
+ * # thetaTauApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('thetaTauApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thetaTauApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thetaTauApp
+ */
+angular.module('thetaTauApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
